4075eb6cc3e52522396e77032a891bc24d74b2b2
Devidas Alhat <devidasalhat029@gmail.com>
2026-07-18 10:45:59 +0530

    updated project

--- up_database.py
+++ up_database.py
@@ -6,7 +6,7 @@ cursor = conn.cursor()
 cursor.execute("ALTER TABLE sensor_record ADD COLUMN start_time TEXT")
 cursor.execute("ALTER TABLE sensor_record ADD COLUMN end_time TEXT")
 cursor.execute("ALTER TABLE sensor_record ADD COLUMN duration TEXT")
-
+cursor.execute("ALTER TABLE sensor_record ADD COLUMN duration TEXT")
 conn.commit()
 conn.close()
 
